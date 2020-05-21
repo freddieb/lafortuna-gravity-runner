33dc93538cf9afd7087cfb664a8ae51a5db7d249
freddieb <freddieiabrown@hotmail.co.uk>
2020-05-21 20:22:34 +0100

    Add start screen

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <util/delay.h>
 #include "lcd.h"
 #include "rotary.h"
+#include <time.h>
+#include <stdlib.h>
 
 #define WIDTH LCDHEIGHT
 #define HEIGHT LCDWIDTH
@@ -41,26 +43,23 @@ void gameOver(void);
 void runGame(void);
 void reset(void);
 void movePlayer(void);
+void displayStartScreen(void);
 
 void main(void) {
     init();
 
-    clear_screen();
-
-    showPlatforms();
-    showPlayer();
-    showObstacles();
+    displayStartScreen();
+    while (get_middle() != 1) {}
 
     while (1) {
-        runGame();
-
-        while (get_middle() != 1) {}
-
+        clear_screen();
         reset();
         showPlatforms();
         showPlayer();
         showObstacles();
         runGame();
+
+        while (get_middle() != 1) {}
     }
 }
 
@@ -72,6 +71,8 @@ void init(void) {
 
     init_lcd();
     init_rotary();
+    
+    srand(time(NULL));   
 }
 
 void reset() {
@@ -89,16 +90,16 @@ void showPlatforms() {
     topPlatform.left = 0;
     topPlatform.right = WIDTH;
     topPlatform.top = 0;
-    topPlatform.bottom = PLATFORM_HEIGHT;
+    topPlatform.bottom = PLATFORM_HEIGHT-1;
 
     bottomPlatform.left = 0;
     bottomPlatform.right = WIDTH;
     int height = HEIGHT;
-    bottomPlatform.top= height-PLATFORM_HEIGHT;
+    bottomPlatform.top= height-PLATFORM_HEIGHT+1;
     bottomPlatform.bottom = HEIGHT;
     
-    fill_rectangle(topPlatform, GREEN);
-    fill_rectangle(bottomPlatform, GREEN);
+    fill_rectangle(topPlatform, WHITE);
+    fill_rectangle(bottomPlatform, WHITE);
 }
 
 void showPlayer() {
@@ -107,7 +108,7 @@ void showPlayer() {
     player.bottom = HEIGHT - PLATFORM_HEIGHT;
     player.top = player.bottom - size;
 
-    fill_rectangle(player, CYAN);
+    fill_rectangle(player, GREEN);
 }
 
 void setPlayerBottom() {
@@ -125,40 +126,48 @@ void setPlayerTop() {
 
     player.top = PLATFORM_HEIGHT;
     player.bottom = player.top + size;
-    fill_rectangle(player, CYAN);
+    fill_rectangle(player, GREEN);
 }
 
 void showObstacles() {
     for (int i = 0; i < OBSTACLE_COUNT; i++) {
+       int r1 = 30 + (rand() % 70);
+
         if (i % 2 == 0) {
             obstacles[i].top = PLATFORM_HEIGHT;
-            obstacles[i].bottom = 50 + PLATFORM_HEIGHT;
+            obstacles[i].bottom = r1 + PLATFORM_HEIGHT;
         } else {
-            obstacles[i].top = HEIGHT - 50 + PLATFORM_HEIGHT;
+            obstacles[i].top = HEIGHT - r1 + PLATFORM_HEIGHT;
             obstacles[i].bottom = HEIGHT - PLATFORM_HEIGHT;
         }
 
-        obstacles[i].left = ((1 + i) * (WIDTH / 4)) - 1;
+        int r2 = (rand() % 40) - 20;
+
+        obstacles[i].left = ((1 + i) * (WIDTH / 4)) + r2;
         obstacles[i].right = obstacles[i].left + OBSTACLE_WIDTH;
 
-        fill_rectangle(obstacles[i], RED);
+        fill_rectangle(obstacles[i], WHITE);
     }
     
 }
 
 void generateNextObstacle(int i) {
+    int r2 = 30 + (rand() % 70);
+
     if (i % 2 == 0) {
         obstacles[i].top = PLATFORM_HEIGHT;
-        obstacles[i].bottom = 50 + PLATFORM_HEIGHT;
+        obstacles[i].bottom = r2 + PLATFORM_HEIGHT;
     } else {
-        obstacles[i].top = HEIGHT - 50 + PLATFORM_HEIGHT;
+        obstacles[i].top = HEIGHT - r2 + PLATFORM_HEIGHT;
         obstacles[i].bottom = HEIGHT - PLATFORM_HEIGHT;
     }
     
-    obstacles[i].right = WIDTH + OBSTACLE_WIDTH;
-    obstacles[i].left = WIDTH;
+    int r1 = (rand() % 40);
 
-    fill_rectangle(obstacles[i], RED);
+    obstacles[i].right = r1 + WIDTH + OBSTACLE_WIDTH;
+    obstacles[i].left = r1 + WIDTH;
+
+    fill_rectangle(obstacles[i], WHITE);
 }
 
 void scrollLoop(uint8_t isBtnClicked) {
@@ -184,11 +193,6 @@ void scrollLoop(uint8_t isBtnClicked) {
     // Increase debounce counter as scroll iteration delay decreases
     if (isBtnClicked && scrollCount > 30 && lastBtnClick < (scrollCount - (12 + (0.03 * scrollCount)))) { 
         isGravityDown = isGravityDown ? 0 : 1;
-        // if (isGravityDown) {
-        //     setPlayerBottom();
-        // } else {
-        //     setPlayerTop();
-        // }
 
         lastBtnClick = scrollCount;
     }
@@ -211,11 +215,11 @@ void scrollLoop(uint8_t isBtnClicked) {
             obstacles[i].right = obstacles[i].right - 1;
 
             fill_rectangle(emptyRect, BLACK);
-            fill_rectangle(obstacles[i], RED);
+            fill_rectangle(obstacles[i], WHITE);
         }
     }
 
-    _delay_ms(15 - (0.01 * scrollCount));
+    _delay_ms(15 - (0.01 * scrollCount)); // causes exponential speed increase
     
 }
 
@@ -236,7 +240,7 @@ void movePlayer() {
         player.top = player.top + stepSize;
 
         fill_rectangle(emptyRect, BLACK);
-        fill_rectangle(player, CYAN);
+        fill_rectangle(player, GREEN);
     } else if (isGravityDown == 1 && player.top != PLATFORM_HEIGHT) {
         // Should move up
         if (player.top - stepSize < PLATFORM_HEIGHT) {
@@ -250,12 +254,12 @@ void movePlayer() {
         player.top = player.top - stepSize;
 
         fill_rectangle(emptyRect, BLACK);
-        fill_rectangle(player, CYAN);
+        fill_rectangle(player, GREEN);
     }
 }
 
 void runGame() {
-    while (!stopGame) {
+    while (stopGame != 1) {
         scrollLoop(get_middle());
     }
 }
@@ -263,26 +267,62 @@ void runGame() {
 void gameOver() {
     stopGame = 1;
     clear_screen();
+
+    display.foreground = GREEN;
     
     display_string("\n\n\n\n");
 
-    display_string("            _____                      \n");
-    display_string("           / ____|                     \n");
-    display_string("          | |  __  __ _ _ __ ___   ___ \n");
-    display_string("          | | |_ |/ _` | '_ ` _ \\ / _ \\\n");
-    display_string("          | |__| | (_| | | | | | |  __/\n");
-    display_string("           \\_____|\\__,_|_| |_| |_|\\___|\n");
+    display_string("             _____                      \n");
+    display_string("            / ____|                     \n");
+    display_string("           | |  __  __ _ _ __ ___   ___ \n");
+    display_string("           | | |_ |/ _` | '_ ` _ \\ / _ \\\n");
+    display_string("           | |__| | (_| | | | | | |  __/\n");
+    display_string("            \\_____|\\__,_|_| |_| |_|\\___|\n");
 
     display_string("\n\n");
 
-    display_string("              ____                 _ \n");
-    display_string("             / __ \\               | |\n");
-    display_string("            | |  | |_   _____ _ __| | \n");
-    display_string("            | |  | \\ \\ / / _ \\ '__| |\n");
-    display_string("            | |__| |\\ V /  __/ |  |_|\n");
-    display_string("             \\____/  \\_/ \\___|_|  (_)\n");
+    display_string("               ____                 _ \n");
+    display_string("              / __ \\               | |\n");
+    display_string("             | |  | |_   _____ _ __| | \n");
+    display_string("             | |  | \\ \\ / / _ \\ '__| |\n");
+    display_string("             | |__| |\\ V /  __/ |  |_|\n");
+    display_string("              \\____/  \\_/ \\___|_|  (_)\n");
 
     display_string("\n\n\n");
 
-    display_string("        Click the middle button to try again");
+    display.foreground = WHITE;
+    display_string("          Click the middle button to try again");
 }
+
+void displayStartScreen() {
+    clear_screen();
+
+    display.foreground = GREEN;
+    
+    display_string("\n\n\n\n");
+
+    display_string("         _____                 _ _         \n");
+    display_string("        / ____|               (_) |        \n");
+    display_string("       | |  __ _ __ __ ___   ___| |_ _   _ \n");
+    display_string("       | | |_ | '__/ _` \\ \\ / / | __| | | |\n");
+    display_string("       | |__| | | | (_| |\\ V /| | |_| |_| |\n");
+    display_string("        \\_____|_|  \\__,_| \\_/ |_|\\__|\\__, |\n");
+    display_string("                                      __/ |\n");
+    display_string("                                     |___/ \n");
+
+    display_string("\n\n");
+
+    display_string("          _____                             \n");
+    display_string("         |  __ \\                            \n");
+    display_string("         | |__) |   _ _ __  _ __   ___ _ __ \n");
+    display_string("         |  _  / | | | '_ \\| '_ \\ / _ \\ '__|\n");
+    display_string("         | | \\ \\ |_| | | | | | | |  __/ |   \n");
+    display_string("         |_|  \\_\\__,_|_| |_|_| |_|\\___|_|   \n");
+
+    display_string("\n\n\n");
+
+    display.foreground = WHITE;
+    display_string("         Click the middle button to start");
+}
+
+                                  
